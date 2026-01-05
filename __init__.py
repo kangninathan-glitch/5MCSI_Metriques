803f82ed83aa3e18bd6f67f584bb6bfd0cfe48d4
kangninathan-glitch <kangninathan@gmail.com>
2026-01-05 17:02:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -40,6 +40,11 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
