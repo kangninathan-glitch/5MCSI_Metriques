522e00c3bbe265c931768ffff867d31b4b667bef
kangninathan-glitch <kangninathan@gmail.com>
2026-01-05 16:49:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,5 +36,10 @@ def meteo():
 
     return jsonify(results=results)
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
